be129d9a48effcc81ecc288caf6e6e316156d0c3
Jonathan M Reiter <unfathomablej@gmail.com>
2019-05-01 14:29:15 -0700

    Persist some sort of token thingy

--- scratchpad.sh
+++ scratchpad.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+DDOG_API=foo
+CLUSTER_TOKEN=bar
+
 # Build base cluster
 minikube start -p ultra
 kubectl create ns telemetry
@@ -10,6 +13,7 @@ helm init # installs to kube-system w/o fancy RBAC
 helm install --name datadog-monitoring \
     --set datadog.apiKey="${DDOG_API}" \
     --set datadog.appKey="${DDOG_API}" \
+    --set clusterAgent.token="${CLUSTER_TOKEN}" \
     --set clusterAgent.enabled=true \
     --set clusterAgent.metricsProvider.enabled=true \
     stable/datadog
