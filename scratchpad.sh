011c4a4197929548b7f5c91114235116a8544b92
Jonathan M Reiter <unfathomablej@gmail.com>
2019-05-01 14:13:08 -0700

    Cluster agent deploy in scratch

--- scratchpad.sh
+++ scratchpad.sh
@@ -1,6 +1,15 @@
 #!/bin/zsh
 
+# Build base cluster
 minikube start -p ultra
-kubectl create secret generic datadog-api --from-literal=token="{$DDOG_API}"
-# kubectl get secrets
+kubectl create ns telemetry
 
+# Deploy base agent image with clustering
+# TODO: Generate token into secrets bucket, manage with CM
+helm init # installs to kube-system w/o fancy RBAC
+helm install --name datadog-monitoring \
+    --set datadog.apiKey="${DDOG_API}" \
+    --set datadog.appKey="${DDOG_API}" \
+    --set clusterAgent.enabled=true \
+    --set clusterAgent.metricsProvider.enabled=true \
+    stable/datadog
